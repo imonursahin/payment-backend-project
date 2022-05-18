44924ab4ceaa43ad963a8ff44ae98275436eea7d
onur_sahin <imonursahin@gmail.com>
2022-05-18 15:30:35 +0300

    withdraw

--- server/routes/withdraw.js
+++ server/routes/withdraw.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const { accounts } = require('./account');
 const router = express.Router();
+const AccountData = require('./account_data');
+
+let accountData = new AccountData().getInstance()
+
 
 // Withdraw  
 router.post('/', (req, res, next) => {
 
+    let accountInfo = accountData.getAccount()
+
     let accountNumber = req.body.accountNumber
     let amount = req.body.amount
+
+    let accountIndex = accountInfo.findIndex(item => item.accountNumber == accountNumber)
+
+    if (accountIndex != null) {
+
+        if (accountInfo[accountIndex].accountType == "individual") {
+            accountInfo[accountIndex].balance -= amount
+            accountData.updateAccount(accountInfo)
+            res.status(200).json({
+                message: "Succesfuly",
+
+            })
+
+        }
+
+    } else {
+        res.status(404).json({
+            message: "Account not found"
+        })
+    }
+
+
 
 
 
